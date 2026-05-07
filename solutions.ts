8b74e51d7117809ec82a36df29dee83cfedf4ce5
Sudipta Sen Gupta <gauravsengupto321@gmail.com>
2026-05-07 17:09:27 +0600

    problem 5 solved

--- solutions.ts
+++ solutions.ts
@@ -82,3 +82,32 @@ const getProperty=<Object,Key extends keyof Object>(object:Object,key:Key):Objec
 // console.log(getProperty({name:'gangaram'},'name'));
 
 
+//problem 5
+//  Define an interface Book with properties title, author, and publishedYear. Create a function toggleReadStatus that accepts a Book object and returns a new object with an added isRead property (boolean), defaulting to true.
+
+// Sample Input:
+// const myBook = { title: "TypeScript Guide", author: "Jane Doe", publishedYear: 2024 };
+// toggleReadStatus(myBook);
+
+// // Sample Output:
+// {
+//   title: "TypeScript Guide",
+//   author: "Jane Doe",
+//   publishedYear: 2024,
+//   isRead: true
+// }
+
+    interface Book{
+              title:string;
+              author:string;
+              publishedYear:number;
+    }
+
+    const toggleReadStatus =<T extends Book>(book:T)=>{
+         return {
+                 ...book,
+                 isRead:true
+         }
+    }
+
+    // console.log(toggleReadStatus({title:'physics',author:'rajib',publishedYear:2034}));
